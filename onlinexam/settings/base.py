0ec6b20bfa150e4f3c4579b076d3ebdd7c553c1c
pushpendra <pushpendra@captalnumbers.com>
2021-10-21 11:28:42 +0530

    settings

--- onlinexam/settings/base.py
+++ onlinexam/settings/base.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'onlinexam.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR,],
+        'DIRS': ['templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
